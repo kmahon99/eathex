5605ca3c861c9a54a6f768036f6e9272ff2c99a1
kmahon99 <kmahon99@gmail.com>
2018-04-17 16:53:56 +0100

    Added exit button/moved OBJ load to external function

--- test1.js
+++ test1.js
@@ -5,24 +5,32 @@ var eathex = {
 			target_element:null,
 			renderer:null,
 			mesh:null,
+			content_loaded:false,
 			ready:false
 			};
 
-			
-function eathex_click(target_id, has_controls=true){
+
+function eathex_click(target_id, has_controls){
     setup();
 }
 
-
+//Remove eathex from the target
 function eathex_exit(){
-	$("#eathex_scripts").remove();
 	eathex.target_element.removeChild(eathex.renderer.domElement);
 	$("#eathex_render_window").width(0);
 	$("#eathex_render_window").height(0);
+	$("#eathex_exit").width(0);
+	$("#eathex_exit").height(0);
+	$("#eathex_overlay").css("background-color","rgba(255,255,255,0)");
+	$("#eathex_exit").on('transitionend webkitTransitionEnd oTransitionEnd', function(){
+		$("#eathex_exit").remove();
+	});
 	$("#eathex_render_window").on('transitionend webkitTransitionEnd oTransitionEnd', function(){
-		$("#eathex_overlay").css("background-color","rgba(255,255,255,0)");
-		$("#eathex_overlay").remove();
+		$("#eathex_scripts").remove();
 		$("#eathex_render_window").remove();
+	});
+	$("#eathex_overlay").on('transitionend webkitTransitionEnd oTransitionEnd', function(){
+		$("#eathex_overlay").remove();
 	});
 }
 
@@ -31,8 +39,22 @@ function eathex_exit(){
 function setup(){
 	$(document.body).prepend("<div id='eathex_scripts'><script src='build/three.js'></script><script src='OrbitControls.js'></script><script src='OBJLoader.js'></script></div>");
 	if (!window.jQuery){ $("#eathex_scripts").prepend("<script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>"); }
-	$(document.body).prepend("<div id='eathex_overlay' onclick='eathex_exit()'></div>");
-	$("#eathex_overlay").css({"background-color":"rgba(0,0,0,0)",
+	$(document.body).prepend("<div id='eathex_overlay'></div>");
+	$(document.body).append("<div id='eathex_exit' onclick='eathex_exit()'></div>");
+	$("#eathex_exit").css({
+								"background-color":"rgba(0,0,0,1)",
+								"position":"fixed",
+								"top":"0px",
+								"right":"0px",
+								"width":"0px",
+								"height":"0px",
+								"transition":"all 0.5s",
+								"-webkit-transition":"all 0.5s"
+	});
+	$("#eathex_exit").height($(window).height()*0.1);
+	$("#eathex_exit").width($(window).height()*0.1);
+	$("#eathex_overlay").css({
+								"background-color":"rgba(0,0,0,0)",
 								"position":"fixed",
 								"top":"0px",
 								"left":"0px",
@@ -41,9 +63,10 @@ function setup(){
 								"transition":"background-color 0.5s",
 								"-webkit-transition":"background-color 0.5s"
 								});
-	$("#eathex_overlay").css("background-color","rgba(0,0,0,0.5)");
+	$("#eathex_overlay").css("background-color","rgba(0,0,0,0.7)");
 	$("#eathex_overlay").prepend("<div id='eathex_render_window'></div>");
-	$("#eathex_render_window").css({"position":"fixed",
+	$("#eathex_render_window").css({
+									"position":"fixed",
 									"background-color":"rgba(83,83,92,1)",
 									"height":"0px",
 									"width":"0px",
@@ -52,28 +75,27 @@ function setup(){
 									"left":"0",
 									"right":"0",
 									"margin":"auto",
+									"box-shadow":"0 0 10px",
 									"transition":"all 0.5s",
 									"-webkit-transition":"all 0.5s"
 									});
-	$("#eathex_render_window").height($(window).height()*.8);
-	$("#eathex_render_window").width($(window).height()*.8);
-	$("#eathex_render_window").on('transitionend webkitTransitionEnd oTransitionEnd', function(){ 
-		eathex.ready = true; 
+	$("#eathex_render_window").height($(window).height()*0.8);
+	$("#eathex_render_window").width($(window).height()*0.8);
+	$("#eathex_render_window").on('transitionend webkitTransitionEnd oTransitionEnd', function(){
+		eathex.ready = true;
 		eathex.target_element = document.getElementById("eathex_render_window");
-		init_3D();
+		init_3D(true);
 		$("#eathex_render_window").off('transitionend webkitTransitionEnd oTransitionEnd');
 	});
 }
 
 //Set up the scene
-function init_3D(has_controls=true){
+function init_3D(has_controls){
 	if(eathex.target_element != null){
 		eathex.scene  = new THREE.Scene();
 		eathex.camera = new THREE.PerspectiveCamera();
-		eathex.camera.position.z = 30; eathex.camera.position.y = 10;
-
+		eathex.camera.position.z = 30;
 		eathex.controls = new THREE.OrbitControls(eathex.camera, eathex.target_element);
-
 		eathex.renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
 		eathex.target_element.appendChild(eathex.renderer.domElement);
 
@@ -86,50 +108,57 @@ function init_3D(has_controls=true){
 		eathex.scene.add(pointLight);
 		eathex.scene.background = new THREE.Color($("#"+eathex.target_element.id).css("background-color"));
 
-		var loader = new THREE.OBJLoader();
-		if(eathex.mesh == null){
-			loader.load( 
-				'test_model.obj', 
-				function(object) {
-				  eathex.scene.add(object);
-				  eathex.mesh = object;
-				},
-				function(xhr) {
-					
-					console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-				},
-				function ( error ) {
-					console.log( 'An error happened' );
-			});
-		}
-		else{ eathex.scene.add(eathex.mesh); }
-
-		function resize() {
-			var width = eathex.target_element.clientWidth;
-			var height = eathex.target_element.clientHeight;
-			
-			eathex.renderer.setSize(width, height);
-			eathex.camera.aspect = width/height;
-		}
-
-		function animate() {
-			requestAnimationFrame(animate);    
-			if(eathex.mesh){ eathex.mesh.rotation.y += 0.1 * Math.PI/180; }
-			eathex.controls.update();
-			eathex.renderer.render(eathex.scene, eathex.camera);
-		}
-
 		$(document).ready(function(){resize();});
-		
+
+		loadOBJ('test_model.obj');
 		animate();
 	}
 	else{ alert("Render window unable to load!"); }
 }
 
 function resize() {
-    var width = eathex.target_element.clientWidth;
-    var height = eathex.target_element.clientHeight;
-    eathex.renderer.setSize(width, height);
+	var width = eathex.target_element.clientWidth;
+	var height = eathex.target_element.clientHeight;
+
+	eathex.renderer.setSize(width, height);
+	eathex.camera.aspect = width/height;
+}
+
+function loadOBJ(filename){
+	if(eathex.ready){
+		if(eathex.mesh == null){
+			var loader = new THREE.OBJLoader();
+			$("#eathex_render_window").prepend("<div id='eathex_load_overlay'></div>");
+			$("#eathex_load_overlay").css({
+				"position":"absolute",
+				"width":"100%",
+				"height":"100%",
+				"background-color":"rgba(0,0,0,1)",
+				"transition":"background-color 0.5s",
+				"-webkit-transition":"background-color 0.5s"
+			});
+			loader.load(
+				filename,
+				function(object){
+					eathex.scene.add(object);
+					eathex.mesh = object;
+				},
+				function(xhr){
+					alert((xhr.loaded/xhr.total*100)+'% loaded');
+				},
+				function(error){
+					console.log("Couldn't load file...");
+			});
+		}
+		else{ eathex.scene.add(eathex.mesh); }
+	}
+}
+
+function animate(){
+	requestAnimationFrame(animate);
+	if(eathex.mesh){ eathex.mesh.rotation.y += 0.1 * Math.PI/180; }
+	eathex.controls.update();
+	eathex.renderer.render(eathex.scene, eathex.camera);
 }
 
 //Manage the window's sizing
